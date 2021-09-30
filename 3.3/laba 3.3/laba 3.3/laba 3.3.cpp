72b99bec8716e2f57cc872ea3eab7a3cf84f9ae2
unknown <demian.malko.ik.2021@lpnu>
2021-09-30 23:35:47 +0300

    3i4

---
+++ 3.3/laba 3.3/laba 3.3/laba 3.3.cpp
@@ -0,0 +1,35 @@
+﻿// < Малько Дем'ян Денисович >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 9
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // zminna x
+	double y; // rezul'tat
+	cout << "x = "; cin >> x;
+	// розгалуження в повній формі
+	if (x <= -7)
+		y = 0;
+	else
+		if (-7 < x && x <= -3)
+			y = x + 7;
+		else
+			if (-3 < x && x <= -2)
+				y = 4;
+			else
+				if (-2 < x && x <= 2)
+					y = x * x;
+				else
+					if (2 < x && x <= 4)
+						y = 8 - 2 * x;
+					else
+						y = 0;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+
+}
---
+++ 3.4/laba 3.4/laba 3.4/laba 3.4.cpp
@@ -0,0 +1,23 @@
+﻿// < Малько Дем'ян Денисович >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 9
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double r; // вхіжний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "r = "; cin >> r;
+	// розалуження в повній формі
+	if ((y >= (x - 1) * (x - 1) && (y * y) <= (r * r) - (x * x) && x >= 0) || (y >= -sqrt(r*r - x*x) && y <= 0 && x <= 0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+
+}
